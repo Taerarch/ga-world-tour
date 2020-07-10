8e010cc1a2e558ea9fadc27a57c63c93fca2dcfc
cesarsan8a <cesarm.sanchezo@gmail.com>
2020-07-10 11:50:45 +1000

    map component

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Map from './Map'
 
 
 function App() {
   return (
     <div className="App">
-
+      <Map />
     </div>
   );
 }
---
+++ src/components/Map.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Map extends Component {
+  render () {
+    return (
+      <h1>Hola</h1>
+    )
+  }
+}
+
+export default Map;
