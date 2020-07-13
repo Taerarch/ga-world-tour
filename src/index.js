5fc2d7495011c858764aef9dc37818d5ef44bb08
behdad setoodegan <behdadsetoodegan@gmail.com>
2020-07-13 15:41:38 +1000

    Working version

--- src/index.js
+++ src/index.js
@@ -3,11 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import Nav from './components/Nav';
-<<<<<<< HEAD
-// eslint-disable-next-line
-import Home from './components/Home';
-=======
->>>>>>> 285862a12beb94435d3f6a388f612004b310c92b
 import Video from './components/Video';
 import * as serviceWorker from './serviceWorker';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
