ba883894d9d1abe9a8884a326189dc44eec27c97
cesarsan8a <cesarm.sanchezo@gmail.com>
2020-07-13 13:49:09 +1000

    location's in state

--- src/components/WorldMap.js
+++ src/components/WorldMap.js
@@ -1,16 +1,65 @@
-import React, { useState } from 'react';
+import React, { useState, Component } from 'react';
 import World from '@svg-maps/world';
 import { SVGMap } from 'react-svg-map';
 import 'react-svg-map/lib/index.css'
+import { getLocationName } from './MapFunctions';
+import 'react-svg-map/src/svg-map.scss'
+import WorldInfo from '../MapUtilities/MapInfo.js'
 
 
-function WorldMap () {
 
-  const [property, setProperty] = useState("pop_est");
+class WorldMap extends Component {
+  constructor() {
+    super();
+    this.state = {
+      allCountries: WorldInfo.worldInfo.locations,
+      country: '',
+      focusedLocation: null,
+    }
+    this._handleCountry = this._handleCountry.bind(this);
+    this._handleMouseOver = this._handleMouseOver.bind(this);
 
-  return (
-    <SVGMap map={World} />
-  );
+    const getAllCountries = () => {
+      console.log(WorldInfo.worldInfo.locations);
+      // this.setState({allCountries: [...this.state.allCountries, ]})
+    }
+
+    getAllCountries()
+  }
+
+  _handleCountry(event) {
+    this.setState({ country: event.target.value });
+  }
+
+  colorCountry() {
+
+  }
+
+  _handleLocationFocus(event) {
+    const focusedLocation = getLocationName(event);
+    this.setState({ focusedLocation: focusedLocation });
+  }
+
+  _handleMouseOver(event) {
+    // console.log(, 'aqui');
+    const focusedLocation = getLocationName(event);
+    this.setState({ focusedLocation : focusedLocation});
+  }
+  // getLocationClassName(location, index) {
+  // // Generate random heat map
+  // return `svg-map__location svg-map__location--heat${index % 4}`;
+  // }
+
+
+  render() {
+    return (
+      <div>
+        <select option={ this.state.allCountries }>
+        </select>
+        <SVGMap map={World} onLocationMouseOver={this._handleMouseOver} />
+      </div>
+    );
+  }
 
 }
 
