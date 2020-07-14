b9cac697f55777ff2601220396c750ec18fdb109
behdad setoodegan <behdadsetoodegan@gmail.com>
2020-07-14 13:55:31 +1000

    pull

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import '../App.css'
 import {Link} from 'react-router-dom'
+import Avatar from 'react-avatar';
+import {storage} from './fire'
+
+
 
 function Nav(props) {
     const navStyle={
         color: 'white'
     }
+    const [dlUrl, setUrl] = React.useState("");
 
+    
     return (
         <nav>
             <h3>GA World Tour</h3>
@@ -20,7 +26,7 @@ function Nav(props) {
                         <li>Profile</li>
                     </Link>) 
                 : null}
-
+                <Avatar avatarRedirectUrl={dlUrl} size="100" round={true} />
                 
             </ul>
         </nav>
