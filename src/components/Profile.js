6fa9d47146e882c1fe8ca72e16c2106bc5bfa793
behdad setoodegan <behdadsetoodegan@gmail.com>
2020-07-15 13:00:34 +1000

    pull

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,14 +1,36 @@
 import React, { Component } from 'react'
 import {Link} from 'react-router-dom'
+import fire from './fire';
 
 
 class Profile extends Component {
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
         this.state = { name: "", number: "" }
+        
+        this.name = fire.database().ref().child('name')
+        this.number = fire.database().ref().child('name')
+        if (this.props.user.uid !== undefined){
+            this.name = fire.database().ref().child(this.props.user.uid).child('name')
+            this.number = fire.database().ref().child(this.props.user.uid).child('number')
+        }
+        
         this._handleChangeName = this._handleChangeName.bind(this);
         this._handleChangeNumber = this._handleChangeNumber.bind(this);
         this._handleSubmit = this._handleSubmit.bind(this);
+      }
+
+      componentDidMount(){
+        this.name.on('value', snap =>{
+            this.setState({
+                name: snap.val()
+            })
+        })
+        this.number.on('value', snap =>{
+            this.setState({
+                number: snap.val()
+            })
+        })
       }
 
       _handleChangeName(event) {
@@ -20,19 +42,25 @@ class Profile extends Component {
       }
       _handleSubmit(event) {
         event.preventDefault();
-        this.props.onSubmit(this.state.band, this.state.year);
+        fire.database().ref().child(this.props.user.uid).child('name').set(this.state.name)
+        fire.database().ref().child(this.props.user.uid).child('number').set(this.state.number)
       }
 
     render() {
         return (
             <div>
                 <h3>Profile page</h3>
+                <br/>
+                <h3>Hello {this.state.name}</h3>
+                {this.state.number 
+                ? (<h4>Your phone number is {this.state.number}</h4>) 
+                : (null)}
                 <form onSubmit={this._handleSubmit}>
-                    <label for="band">Full name: </label>
-                    <input type="text" id="name" name="name" onChange={this._handleChangeName} value={this.state.content} required></input>
+                    <label>Full name: </label>
+                    <input type="text" id="name" name="name" onChange={this._handleChangeName} value={this.state.content}></input>
 
-                    <label for="year">Phone number: </label>
-                    <input type="tel" id="number" name="phone" onChange={this._handleChangeNumber} value={this.state.content} required></input>
+                    <label>Phone number: </label>
+                    <input type="tel" id="number" name="phone" onChange={this._handleChangeNumber} value={this.state.content}></input>
 
                     <input type="submit" value="Edit"></input>
                 </form>
