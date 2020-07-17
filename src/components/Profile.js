58e26fecbb39b82412b007001aec2f763a81c32d
cesarsan8a <cesarm.sanchezo@gmail.com>
2020-07-17 10:56:32 +1000

    indentation and map comments

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -5,98 +5,98 @@ import {storage} from './fire'
 import {Card, Button} from 'react-bootstrap'
 
 class Profile extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { name: "", number: "", url: "" }
-        this.name = fire.database().ref().child('name')
-        this.number = fire.database().ref().child('name')
-        if (this.props.user.uid !== undefined){
-            this.name = fire.database().ref().child(this.props.user.uid).child('name')
-            this.number = fire.database().ref().child(this.props.user.uid).child('number')
-        }
-        
-        this._handleChangeName = this._handleChangeName.bind(this);
-        this._handleChangeNumber = this._handleChangeNumber.bind(this);
-        this._handleSubmit = this._handleSubmit.bind(this);
-        let downloadTask = null;
-        if (props.user !== null){
-            downloadTask = storage.ref(`images/${props.user.uid}`);
-        }
-
-        if (downloadTask !== null){
-            this.state.url = downloadTask.getDownloadURL()
-        }
-        
-          
-      }
-
-      componentDidMount(){
-        this.name.on('value', snap =>{
-            this.setState({
-                name: snap.val()
-            })
-        })
-        this.number.on('value', snap =>{
-            this.setState({
-                number: snap.val()
-            })
-        })
-      }
-
-      _handleChangeName(event) {
-        this.setState({name: event.target.value});
-      }
-    
-      _handleChangeNumber(event) {
-        this.setState({number: event.target.value})
-      }
-      _handleSubmit(event) {
-        event.preventDefault();
-        fire.database().ref().child(this.props.user.uid).child('name').set(this.state.name)
-        fire.database().ref().child(this.props.user.uid).child('number').set(this.state.number)
-      }
-
-    render() {
-        return (
-            <div className="profile-page">
-                <div>    
-                    <Card style={{ width: '18rem' }}>
-                        <Card.Img variant="top" src={this.state.url.i} />
-                        <Card.Body>
-                            <Card.Title>{this.state.name}</Card.Title>
-                            <Card.Text>
-                                Phone number : {this.state.number}
-                            </Card.Text>
-                            <Link to='/video'>
-                                <Button className="btn btn-info" >Take a picture</Button>
-                            </Link>
-                        </Card.Body>
-                    </Card>
-                </div>
-                <div>
-                    <br/>
-                    <h3>Hello {this.state.name}</h3>
-                    {this.state.number 
-                    ? (<h4>Your phone number is {this.state.number}</h4>) 
-                    : (null)}
-                    <form onSubmit={this._handleSubmit}>
-                        <div>
-                            <label>Full name:</label>
-                            <input type="text" id="name" name="name" onChange={this._handleChangeName} value={this.state.content}></input>
-                        </div>
-                        <div>
-                            <label>Phone number: </label>
-                            <input type="tel" id="number" name="phone" onChange={this._handleChangeNumber} value={this.state.content}></input>
-                        </div>
-                        <div>
-                            <input type="submit" value="Edit"></input>
-                        </div>
-                        
-                    </form>
-                </div>
-            </div>
-        )
+  constructor(props) {
+    super(props);
+    this.state = { name: "", number: "", url: "" }
+    this.name = fire.database().ref().child('name')
+    this.number = fire.database().ref().child('name')
+    if (this.props.user.uid !== undefined){
+      this.name = fire.database().ref().child(this.props.user.uid).child('name')
+      this.number = fire.database().ref().child(this.props.user.uid).child('number')
     }
+
+    this._handleChangeName = this._handleChangeName.bind(this);
+    this._handleChangeNumber = this._handleChangeNumber.bind(this);
+    this._handleSubmit = this._handleSubmit.bind(this);
+    let downloadTask = null;
+    if (props.user !== null){
+      downloadTask = storage.ref(`images/${props.user.uid}`);
+    }
+
+    if (downloadTask !== null){
+      this.state.url = downloadTask.getDownloadURL()
+    }
+  }
+
+  componentDidMount(){
+    this.name.on('value', snap =>{
+      this.setState({
+        name: snap.val()
+      })
+    })
+
+    this.number.on('value', snap =>{
+      this.setState({
+        number: snap.val()
+      })
+    })
+  }
+
+  _handleChangeName(event) {
+    this.setState({name: event.target.value});
+  }
+
+  _handleChangeNumber(event) {
+    this.setState({number: event.target.value})
+  }
+
+  _handleSubmit(event) {
+    event.preventDefault();
+    fire.database().ref().child(this.props.user.uid).child('name').set(this.state.name)
+    fire.database().ref().child(this.props.user.uid).child('number').set(this.state.number)
+  }
+
+  render() {
+    return (
+      <div className="profile-page">
+        <div>
+            <Card style={{ width: '18rem' }}>
+              <Card.Img variant="top" src={this.state.url.i} />
+              <Card.Body>
+                <Card.Title>{this.state.name}</Card.Title>
+                <Card.Text>
+                  Phone number : {this.state.number}
+                </Card.Text>
+                <Link to='/video'>
+                  <Button className="btn btn-info" >Take a picture</Button>
+                </Link>
+              </Card.Body>
+            </Card>
+        </div>
+        <div>
+          <br/>
+          <h3>Hello {this.state.name}</h3>
+          {this.state.number
+          ? (<h4>Your phone number is {this.state.number}</h4>)
+          : (null)}
+          <form onSubmit={this._handleSubmit}>
+            <div>
+              <label>Full name:</label>
+                <input type="text" id="name" name="name" onChange={this._handleChangeName} value={this.state.content}></input>
+            </div>
+            <div>
+              <label>Phone number: </label>
+              <input type="tel" id="number" name="phone" onChange={this._handleChangeNumber} value={this.state.content}></input>
+            </div>
+            <div>
+              <input type="submit" value="Edit"></input>
+            </div>
+
+          </form>
+          </div>
+      </div>
+    )
+  }
 }
 
 export default Profile
--- src/components/WorldMap.js
+++ src/components/WorldMap.js
@@ -2,12 +2,12 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import World from '@svg-maps/world';
 import { SVGMap } from 'react-svg-map';
-import 'react-svg-map/lib/index.css'
+import 'react-svg-map/lib/index.css';
 import { getLocationName } from './MapFunctions';
-import 'react-svg-map/src/svg-map.scss'
-import WorldInfo from '../MapUtilities/MapInfo.js'
-import '../App.css'
-import _ from 'underscore'
+import 'react-svg-map/src/svg-map.scss';
+import WorldInfo from '../MapUtilities/MapInfo.js';
+import '../App.css';
+import _ from 'underscore';
 
 
 
@@ -15,23 +15,18 @@ class WorldMap extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      allCountries: WorldInfo.worldInfo.locations,
-      country: '',
-      focusedLocation: '',
-      pointedLocation: null,
-      tooltipStyle: {
+      allCountries: WorldInfo.worldInfo.locations, // Info of all the countries on the map
+      pointedLocation: null, // Pointed location by the mouse.
+      tooltipStyle: { // Style of the info hover box.
         display: 'none',
       }
     };
 
-    this._handleCountry = this._handleCountry.bind(this);
     this._handleMouseOver = this._handleMouseOver.bind(this);
     this._handleLocationMouseOut = this._handleLocationMouseOut.bind(this);
-    this._handleOnChange = this._handleOnChange.bind(this);
     this._handleColorCountry = this._handleColorCountry.bind(this);
     this._mapTourCountry = this._mapTourCountry.bind(this);
     this._handleLocationMouseMove = this._handleLocationMouseMove.bind(this);
-    this.getLocationClassName = this.getLocationClassName.bind(this);
   }
 
   componentDidMount() {
@@ -41,11 +36,7 @@ class WorldMap extends Component {
     this.props.onRef(undefined)
   }
 
-  _handleCountry(event) {
-    this.setState({ country: event.target.value });
-  }
-
-  _handleLocationMouseOut(event) {
+  _handleLocationMouseOut(event) { // Turns off the info hover box and sets the pointedLocation to null
     this.setState({
       pointedLocation: null,
       tooltipStyle: { display: 'none'}
@@ -53,7 +44,8 @@ class WorldMap extends Component {
   }
 
   _handleLocationMouseMove(event) {
-    const tooltipStyle = {
+    const tooltipStyle = { // When the mouse is on the map it displays the info hover box
+      // and it sets its position on the screen.
       position: 'fixed',
       display: 'block',
       top: event.clientY + 10,
@@ -62,39 +54,25 @@ class WorldMap extends Component {
     this.setState({ tooltipStyle })
   }
 
-  getLocationClassName(location, index) {
-    console.log(location, index, 'aqui');
-    return ('svg-map__location');
-  }
-
-  _handleOnChange(selectedNodes) {
-		this.setState(prevState => {
-			return {
-				...prevState,
-				selectedLocations: selectedNodes.map(node => node.attributes.name.value)
-			};
-		});
-	}
-
   _handleColorCountry(tourCountry) {
     let allCountries = this.state.allCountries.find(country => country.name === tourCountry)
     if (allCountries) {
       const country = document.getElementById(allCountries.id); //get the toured countries,
-      country.style.fill = 'orange';
+      country.style.fill = 'orange'; // and paints them orange.
     }
   }
 
-  formatYear(string) {
+  formatYear(string) { // formats the date to get the year from the API
     var options = { year: 'numeric' };
     return new Date(string).toLocaleDateString([], options);
   }
 
-  _handleMouseOver(event) {
+  _handleMouseOver(event) { //set the state of pointedLocation and filters the concerts for the hover info.
     const pointedLocation = getLocationName(event);
     const filterYear = this.props.tourCountries.filter( (tour) => this.formatYear(tour.datetime) === this.props.year);
     const filterCountry = _.filter(filterYear, (tour) => {
       if (tour.venue.country === pointedLocation) {
-        return tour
+        return tour //only when the mouse is over a country with concerts it will return their info
       }}
     );
     this.setState({
@@ -102,7 +80,7 @@ class WorldMap extends Component {
     });
   }
 
-  formatDate(string) {
+  formatDate(string) { // formats date into dd/mmmm/yyyy
     var options = { year: 'numeric', month: 'long', day: 'numeric' };
     return new Date(string).toLocaleDateString([], options);
   }
@@ -110,15 +88,15 @@ class WorldMap extends Component {
   _mapTourCountry() {
     const emptyMap = () => {
       const greenWorld = this.state.allCountries
-      // The map function will iterate every country in the world and will paint it green.
+      // The map function will iterate every country in the world and will paint it green (UPDATE: gray because global warming).
       greenWorld.map( (country) => document.getElementById(country.id).style.fill = '#ccc' )
     }
 
-    emptyMap(); // painting the world green before a new search.
+    emptyMap(); // empties the map for a new search.
 
-    const findYear = this.props.tourCountries.filter((tour) => this.formatYear(tour.datetime) === this.props.year)
-    const countryArray = findYear.map((tour) => tour.venue.country)
-    const filterCountryArray = _.uniq(countryArray)
+    const findYear = this.props.tourCountries.filter((tour) => this.formatYear(tour.datetime) === this.props.year) //filters only the concerts from that year.
+    const countryArray = findYear.map((tour) => tour.venue.country) // returns an array with the countries
+    const filterCountryArray = _.uniq(countryArray) // gets rid of the duplicate countries
 
     const colourCountries = (index = 0) => {
       if (index !== filterCountryArray.length) {
@@ -126,21 +104,27 @@ class WorldMap extends Component {
         setTimeout(() => colourCountries(index + 1), 1000)
       }
     }
-    colourCountries();
+    colourCountries(); // using recursion colourCountries passes one country at a time to _handleColorCountry to paint them orange.
   }
+
   render() {
     return (
       <div id="mapDiv">
-        <SVGMap map={World}
-          getLocationClassName={this.getLocationClassName}
-          onChange={this._handleOnChange}
+        <SVGMap map={World} // Select which map you want to use.
+          // the Mouse functions come with react-svg-map package
           onLocationMouseOver={this._handleMouseOver}
           onLocationMouseOut={this._handleLocationMouseOut}
           onLocationMouseMove={this._handleLocationMouseMove}
         />
         <div className="examples__block__map__tooltip" style={this.state.tooltipStyle}>
-          {this.props.tourCountries.filter((t) => this.formatYear(t.datetime) === this.props.year && t.venue.country === this.state.pointedLocation).map((t_filtered) => {
-            return <p>{t_filtered.venue.city}, {t_filtered.venue.country}<br></br> {this.formatDate(t_filtered.datetime)} <br></br>
+          {this.props.tourCountries
+            // filters the info for the info hover block
+            .filter((t) => this.formatYear(t.datetime) === this.props.year && t.venue.country === this.state.pointedLocation)
+            .map((t_filtered) => {
+            return <p>{t_filtered.venue.name}, {t_filtered.venue.city}
+              <br></br>
+               {this.formatDate(t_filtered.datetime)}
+              <br></br>
             </p>
           })}
         </div>
